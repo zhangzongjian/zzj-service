874f38345182eb2563fa706a397dc03437b8da10
zhangzongjian <1102349546@qq.com>
2023-04-14 01:50:54 +0800

    【zutil.sh】

--- kill_8080.bat
+++ kill_8080.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Kill the process running on the specified port
-FOR /F "tokens=5" %%P IN ('netstat -a -n -o ^| findstr :8080') DO TaskKill.exe /PID %%P /F
+FOR /F "tokens=5" %%P IN ('netstat -a -n -o ^| findstr :8080 ^| findstr LISTENING') DO TaskKill.exe /PID %%P /F
 echo 0
